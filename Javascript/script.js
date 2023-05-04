3ff4439581c0d11f94a356453de77e769e101418
Ahmed Adel <70106396+ahmed23-m@users.noreply.github.com>
2023-05-04 18:44:04 +0200

    Uploading main files

---
+++ Javascript/script.js
@@ -0,0 +1,93 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDjan3X86EnK2UCibt6SO39yo66MDABK0A",
+    authDomain: "noro-14113.firebaseapp.com",
+    databaseURL: "https://noro-14113-default-rtdb.firebaseio.com",
+    projectId: "noro-14113",
+    storageBucket: "noro-14113.appspot.com",
+    messagingSenderId: "761578432013",
+    appId: "1:761578432013:web:175bc5a9ef337580a75744",
+    measurementId: "G-WPM8CBFMJJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+var Signals;
+var details = document.querySelector(".Details p");
+
+import { getDatabase, get, ref, child, onValue } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+const database = getDatabase();
+onValue(child(ref(database), "Signals"), (snapshot) => {
+    Signals = snapshot.val();
+    // console.log(Signals);
+});
+onValue(child(ref(database), "The Ratio"), (snapshot) => {
+    details.textContent = snapshot.val();
+});
+const ctx = document.getElementById('myChart');
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [0],
+        datasets: [
+            {
+                label: 'Signals speed(HZ)',
+                data: [0],
+                borderWidth: 1,
+                pointRadius: 0
+            }
+        ]
+    },
+    options: {
+        scales: {
+            x: {
+                display: true,
+                title: {
+                    display: true,
+                    text: 'Time(seconds)'
+                }
+            },
+            y: {
+                display: true,
+                title: {
+                    display: true,
+                    text: 'Signals speed(HZ)'
+                }
+            }
+        }
+    }
+});
+window.setInterval(mycallback, 1000);
+// var counter =0;
+var i =0;
+// var ok = true;
+function mycallback() {
+    // var Time = new Date();
+    // Time = Time.getSeconds();
+    // var t =Number(Time);
+    // var sec = 60;
+    // if(t===0&&ok===true)
+    // {
+    //     counter++;
+    //     ok = false;
+    // }
+    // ok = true;
+    console.log(i);
+    i++;
+    // console.log(t+(sec*counter));
+    // myChart.data.labels.push((t+(sec*counter)));
+    myChart.data.labels.push(i);
+    myChart.data.datasets[0].data.push(Signals);
+    if(myChart.data.labels.length>20)
+    {
+        myChart.data.labels.splice(0,4);
+        myChart.data.datasets[0].data.splice(0,4);
+    }
+    myChart.update();
+}
